9c7bc7c1d55f185794de70d13b45e6b1f1ade611
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-05-04 21:14:46 +0700

    Change typo in prompt message and change to ===
    There was typo in prompt message "... paper of scissors" changed
    "of" to "or". Changed all == compares to ===.

--- javascript.js
+++ javascript.js
@@ -3,10 +3,10 @@ function getComputerChoice() {
 
     let randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    if (randomNumber == 1) {
+    if (randomNumber === 1) {
         return "rock";
     }
-    else if (randomNumber == 2) {
+    else if (randomNumber === 2) {
         return "paper";
     }
     else {
@@ -19,7 +19,7 @@ function getHumanChoice() {
     // Returns string with choice by user
 
     let userChoice = "";
-    userChoice = prompt("Enter your choice of rock, paper of scissors");
+    userChoice = prompt("Enter your choice of rock, paper or scissors");
     let loweredUserChoice = userChoice.toLowerCase();
 
     return loweredUserChoice;
@@ -59,30 +59,30 @@ function playGame() {
         // Plays a whole rock, paper, scissors round.
         // Increments score of winner
 
-        if (humanChoice == computerChoice) {
+        if (humanChoice === computerChoice) {
             console.log(`Draw! ${capitalizeString(humanChoice)} = ${capitalizeString(computerChoice)}!`);
         }
-        else if (humanChoice == "rock" && computerChoice == "scissors") {
+        else if (humanChoice === "rock" && computerChoice === "scissors") {
             console.log("You win! Rock beats Scissors!");
             humanScore++;
         }
-        else if (humanChoice == "rock" && computerChoice == "paper") {
+        else if (humanChoice === "rock" && computerChoice === "paper") {
             console.log("You lose! Paper beats Rock!");
             computerScore++;
         }
-        else if (humanChoice == "paper" && computerChoice == "rock") {
+        else if (humanChoice === "paper" && computerChoice === "rock") {
             console.log("You win! Paper beats Rock!");
             humanScore++;
         }
-        else if (humanChoice == "paper" && computerChoice == "scissors") {
+        else if (humanChoice === "paper" && computerChoice === "scissors") {
             console.log("You lose! Scissors beat Paper!");
             computerScore++;
         }
-        else if (humanChoice == "scissors" && computerChoice == "paper") {
+        else if (humanChoice === "scissors" && computerChoice === "paper") {
             console.log("You win! Scissors beat Paper!");
             humanScore++;
         }
-        else if (humanChoice == "scissors" && computerChoice == "rock") {
+        else if (humanChoice === "scissors" && computerChoice === "rock") {
             console.log("You lose! Rock beats Scissors!");
             computerScore++;
         }
