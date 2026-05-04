2ebc535bc5a788e5b07180d290dea2cc30901157
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-05-04 21:00:25 +0700

    Add playGame function
    playGame Function starts a whole game with 5 rounds. Put score variables
    in playGame function. Put playRound function within playGame function,
    so it can access score variables.

--- javascript.js
+++ javascript.js
@@ -34,42 +34,52 @@ function capitalizeString(string) {
 }
 
 
-function playRound(humanChoice, computerChoice) {
-    // Plays a whole rock, paper, scissors round.
-    // Increments score of winner
+function playGame() {
+    // Plays a complete game with 5 rounds
 
-    if (humanChoice == computerChoice) {
-        console.log(`Draw! ${capitalizeString(humanChoice)} = ${capitalizeString(computerChoice)}!`);
+    function playRound(humanChoice, computerChoice) {
+        // Plays a whole rock, paper, scissors round.
+        // Increments score of winner
+
+        if (humanChoice == computerChoice) {
+            console.log(`Draw! ${capitalizeString(humanChoice)} = ${capitalizeString(computerChoice)}!`);
+        }
+        else if (humanChoice == "rock" && computerChoice == "scissors") {
+            console.log("You win! Rock beats Scissors!");
+            humanScore++;
+        }
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            console.log("You lose! Paper beats Rock!");
+            computerScore++;
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+            console.log("You win! Paper beats Rock!");
+            humanScore++;
+        }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
+            console.log("You lose! Scissors beat Paper!");
+            computerScore++;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+            console.log("You win! Scissors beat Paper!");
+            humanScore++;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+            console.log("You lose! Rock beats Scissors!");
+            computerScore++;
+        }
     }
-    else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win! Rock beats Scissors!");
-        humanScore++;
-    }
-    else if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! Paper beats Rock!");
-        computerScore++;
-    }
-    else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! Paper beats Rock!");
-        humanScore++;
-    }
-    else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! Scissors beat Paper!");
-        computerScore++;
-    }
-    else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! Scissors beat Paper!");
-        humanScore++;
-    }
-    else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose! Rock beats Scissors!");
-        computerScore++;
+
+    let humanScore = 0;
+    let computerScore = 0;
+    let humanChoice = "";
+    let computerChoice = "";
+
+
+    for (let i = 0; i < 5; i++) {
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
     }
 }
-
-humanScore = 0;
-computerScore = 0;
-
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
